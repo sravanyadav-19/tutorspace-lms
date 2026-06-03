153fa284d5f7680fa82e3e4817beafe16791a3c4
sravanyadav-19 <sravanyadav790@gmail.com>
2026-06-03 20:21:45 +0530

    deploy: inline API base URL, remove variable ref

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-const API_BASE = 'https://tutorspace-lms.onrender.com/api'
-
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: 'https://tutorspace-lms.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 })
 
